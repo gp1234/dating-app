91657cd302308f3a12fc10a5dfb23cc24e02cb61
Giovanni Poveda <giovanni_povedda@hotmail.com>
2019-10-27 18:10:45 -0500

    End of section 3

---
+++ DatingApp/Data/IAuthRepository.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using DatingApp.Models;
+
+namespace DatingApp.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User user, string password);
+         Task<User> Login(string username, string password);
+         Task<bool> UserExistes(string username);
+         
+         
+    }
+}
